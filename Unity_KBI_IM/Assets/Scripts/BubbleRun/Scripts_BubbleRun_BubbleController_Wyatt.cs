73a35837d324dcd3dc74ad28e8ff31859ecc5d3d
Wyatt Marvil <2020wmarvil@gmail.com>
2021-10-04 21:35:16 -0400

    Gyro input for BubbleRun Controller

--- Unity_KBI_IM/Assets/Scripts/BubbleRun/Scripts_BubbleRun_BubbleController_Wyatt.cs
+++ Unity_KBI_IM/Assets/Scripts/BubbleRun/Scripts_BubbleRun_BubbleController_Wyatt.cs
@@ -17,27 +17,50 @@ public class Scripts_BubbleRun_BubbleController_Wyatt : MonoBehaviour {
 
         Screen.orientation = ScreenOrientation.LandscapeLeft;
 
-        pw(AttitudeSensor.current);
-
         if (AttitudeSensor.current != null) {
             InputSystem.EnableDevice(AttitudeSensor.current);
         }
 	}
 
 	private void Update() {
-        pw(AttitudeSensor.current.attitude.ReadValue());
+        // y corresponds to forward
+            // flat is at zero degrees
+            // 360 -> 270 is forward
+            // 0 -> 90 is backward
 
-        if (Keyboard.current[Key.W].wasPressedThisFrame) {
-            input.y += 1;
+        Quaternion attitude = AttitudeSensor.current.attitude.ReadValue();
+        pw("x: " + attitude.eulerAngles.x + "\ny: " + attitude.eulerAngles.y + "\nz: " + attitude.eulerAngles.z);
+
+        float attitudeX = attitude.eulerAngles.x;
+        float attitudeY = attitude.eulerAngles.y;
+
+        //if (Keyboard.current[Key.W].wasPressedThisFrame) {
+        //    input.y += 1;
+		//}
+        //if (Keyboard.current[Key.S].wasPressedThisFrame) {
+        //    input.y -= 1;
+		//}
+        //if (Keyboard.current[Key.A].wasPressedThisFrame) {
+        //    input.x -= 1;
+		//}
+        //if (Keyboard.current[Key.D].wasPressedThisFrame) {
+        //    input.x += 1;
+		//}
+
+        if (attitudeX < 355f && attitudeX > 270f) {
+            input.x = 1f; // forward
+		} else if (attitudeX > 5f && attitudeX < 90f) {
+            input.x = -1f; // backward
+		} else {
+            input.x = 0f; // stop
 		}
-        if (Keyboard.current[Key.S].wasPressedThisFrame) {
-            input.y -= 1;
-		}
-        if (Keyboard.current[Key.A].wasPressedThisFrame) {
-            input.x -= 1;
-		}
-        if (Keyboard.current[Key.D].wasPressedThisFrame) {
-            input.x += 1;
+
+        if (attitudeY < 355f && attitudeY > 270f) {
+            input.y = 1f; // forward
+		} else if (attitudeY > 5f && attitudeY < 90f) {
+            input.y = -1f; // backward
+		} else {
+            input.y = 0f; // stop
 		}
 
         input = input.normalized;
@@ -47,11 +70,14 @@ public class Scripts_BubbleRun_BubbleController_Wyatt : MonoBehaviour {
         Vector3 movement = new Vector3(input.x, 0f, input.y);
 
         Vector3 cameraDir = transform.position - cam.transform.position;
+        cameraDir.y = 0f;
 
-        Vector3 moveDir = new Vector3(movement.x * cameraDir.x, 0f, movement.y * cameraDir.y);
+        Vector3 moveDir = new Vector3(movement.x, 0f, movement.z);
+        pw("movement: " + moveDir);
 
-        float force = 10;
-        rb.AddForce(moveDir * force * Time.fixedDeltaTime);
-        
+        float force = 60;
+        float speed = 7f;
+        //rb.AddForce(moveDir * force * Time.fixedDeltaTime);
+        rb.velocity = moveDir * speed;
     }
 }
