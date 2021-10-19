b25cfb1f0edb5dde3341292e5f8f3f633f77a592
BianoBonzo <brianlin16@hotmail.com>
2021-10-19 14:36:46 -0400

    Created moving fan obstacle

--- Unity_KBI_IM/Assets/Scripts/Minigames/MiniGolf/Scripts_WindArea_MiniGolf_BrianLin.cs
+++ Unity_KBI_IM/Assets/Scripts/Minigames/MiniGolf/Scripts_WindArea_MiniGolf_BrianLin.cs
@@ -6,7 +6,7 @@ public class Scripts_WindArea_MiniGolf_BrianLin : MonoBehaviour
 {
     public float windForce; 
     public Vector3 direction; 
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +16,7 @@ public class Scripts_WindArea_MiniGolf_BrianLin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Mathf.PingPong(Time.time, 2) * 3 - 24;
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);        
     }
 }
