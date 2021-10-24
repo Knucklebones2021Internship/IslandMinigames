a20b6040531cafc3908944d7f890b94a2ed2b393
Wyatt Marvil <2020wmarvil@gmail.com>
2021-10-23 23:53:11 -0400

    Procedural geometry basics

---
+++ Unity_KBI_IM/Assets/Scripts/Minigames/BubbleRun/ProcGeo/Scripts_QuadGenerator_Wyatt.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scripts_QuadGenerator_Wyatt : MonoBehaviour {
+	private void Awake() {
+		Mesh mesh = new Mesh();
+		mesh.name = "Procedural Mesh";
+
+		List<Vector3> points = new List<Vector3>() {
+			new Vector3(-0.5f,  0.5f,  0f),
+			new Vector3( 0.5f,  0.5f,  0f),
+			new Vector3(-0.5f, -0.5f,  0f),
+			new Vector3( 0.5f, -0.5f,  0f)
+		};
+
+		int[] triIndices = new int[] {
+			1, 0, 2,
+			3, 1, 2,
+		};
+
+		List<Vector2> uvs = new List<Vector2>() {
+			new Vector2(1, 1),
+			new Vector2(0, 1),
+			new Vector2(1, 0),
+			new Vector2(0, 0)
+		};
+
+		List<Vector3> normals = new List<Vector3>() {
+			new Vector3(0, 0, 1),
+			new Vector3(0, 0, 1),
+			new Vector3(0, 0, 1),
+			new Vector3(0, 0, 1)
+		};
+
+		mesh.SetVertices(points);
+		mesh.triangles = triIndices;
+		mesh.SetNormals(normals);
+		mesh.SetUVs(0, uvs);
+
+		GetComponent<MeshFilter>().sharedMesh = mesh;
+	}
+}
