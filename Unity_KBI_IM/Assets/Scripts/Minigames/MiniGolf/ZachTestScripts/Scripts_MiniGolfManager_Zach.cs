caa5b0bb0c3099f095e6a6fb780cadf5eea27cc6
Zachariah Abueg <76718657+zdgra@users.noreply.github.com>
2021-10-30 23:57:26 -0400

    first drag + shoot prototype inside new test lab scene

--- Unity_KBI_IM/Assets/Scripts/Minigames/MiniGolf/ZachTestScripts/Scripts_MiniGolfManager_Zach.cs
+++ Unity_KBI_IM/Assets/Scripts/Minigames/MiniGolf/ZachTestScripts/Scripts_MiniGolfManager_Zach.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// NO LONGER IN USE
+// USING OLD INPUT SYSTEM NOW
+
 [DefaultExecutionOrder(-50)]
 public class Scripts_MiniGolfManager_Zach : Scripts_BaseManager_Wyatt
 {
--- Unity_KBI_IM/Assets/Scripts/Minigames/MiniGolf/ZachTestScripts/Scripts_MiniGolf_BallController_Zach.cs
+++ Unity_KBI_IM/Assets/Scripts/Minigames/MiniGolf/ZachTestScripts/Scripts_MiniGolf_BallController_Zach.cs
@@ -1,46 +1,123 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.EnhancedTouch;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Scripts_MiniGolf_BallController_Zach : MonoBehaviour
 {
-    /*[SerializeField] private LineRenderer lineRenderer;
-    [SerializeField] private GameObject areaAffector;
-    [SerializeField] private float maxForce, forceModifier;*/
+    [SerializeField] private float shotPower;
+    [SerializeField] private float stopVelocity = 0.05f;
 
-    private float force;
+    [SerializeField] private LineRenderer lineRenderer;
+
+    private bool isIdle;
+    private bool isAiming;
+
     private Rigidbody rb;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+
+        isAiming = false;
+        lineRenderer.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        Debug.Log("touch position: " + Scripts_InputManager_Wyatt.touchPosition.ReadValue<Vector2>());
+        if (rb.velocity.magnitude < stopVelocity)
+            Stop();
+
+        if (isIdle)
+            if (Input.touchCount > 0)
+                isAiming = true;
+
+        ProcessAim();
     }
 
-    private void OnEnable()
+    /*private void OnMouseDown()
     {
-        // when input first happens - for instance, 
-        Scripts_InputManager_Wyatt.touchPress.started += Shoot;
+        if (isIdle)
+            isAiming = true;
+    }*/
+
+    private void ProcessAim()
+    {
+        if (!isAiming || !isIdle)
+            return;
+
+        Vector3? worldPoint = CastMouseClickRay();
+
+        // Debug.Log("World Point/Ray: " + worldPoint);
+
+        if (!worldPoint.HasValue)
+            return;
+
+        DrawLine(worldPoint.Value);
+
+        if (Input.touchCount > 0)
+            if (Input.touches[0].phase == TouchPhase.Ended)
+                Shoot(worldPoint.Value);
     }
 
-    private void OnDisable()
+    private void Shoot(Vector3 worldPoint)
     {
-        Scripts_InputManager_Wyatt.touchPress.started -= Shoot;
+        isAiming = false;
+        lineRenderer.enabled = false;
+
+        Vector3 horizontalWorldPoint = new Vector3(worldPoint.x, transform.position.y, worldPoint.z);
+
+        Vector3 direction = (horizontalWorldPoint - transform.position).normalized;
+        float strength = Vector3.Distance(transform.position, horizontalWorldPoint);
+
+        rb.AddForce(direction * strength * shotPower);
+
+        isIdle = false;
     }
 
-    void Shoot(InputAction.CallbackContext context)
+    private void DrawLine(Vector3 worldPoint)
     {
-        Debug.Log("Shoot");
+        Vector3[] positions = { transform.position, worldPoint };
+        lineRenderer.SetPositions(positions);
+        lineRenderer.enabled = true;
+    }
 
-        print("zach position: " + Scripts_InputManager_Wyatt.touchPosition.ReadValue<Vector2>());
-        print("zach shoot!");
+    private void Stop()
+    {
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        isIdle = true;
+    }
+
+    private Vector3? CastMouseClickRay()
+    {
+        /*Vector3 screenMousePositionFar = new Vector3(
+            Input.mousePosition.x,                                                                  // input
+            Input.mousePosition.y,                                                                  // input
+            Camera.main.farClipPlane);
+        Vector3 screenMousePositionNear = new Vector3(
+            Input.mousePosition.x,                                                                  // input
+            Input.mousePosition.y,                                                                  // input
+            Camera.main.nearClipPlane);*/
+
+        if (Input.touchCount > 0)
+        {
+            Vector3 screenMousePositionFar = new Vector3(
+                Input.GetTouch(0).position.x,
+                Input.GetTouch(0).position.y,
+                Camera.main.farClipPlane);
+            Vector3 screenMousePositionNear = new Vector3(
+                Input.GetTouch(0).position.x,
+                Input.GetTouch(0).position.y,
+                Camera.main.nearClipPlane);
+                 Vector3 worldMousePositionFar = Camera.main.ScreenToWorldPoint(screenMousePositionFar);
+            Vector3 worldMousePositionNear = Camera.main.ScreenToWorldPoint(screenMousePositionNear);
+            RaycastHit hit;
+            if (Physics.Raycast(worldMousePositionNear, worldMousePositionFar - worldMousePositionNear, out hit, float.PositiveInfinity))
+                return hit.point;
+            else
+                return null;
+        }
+        return null;
     }
 }
