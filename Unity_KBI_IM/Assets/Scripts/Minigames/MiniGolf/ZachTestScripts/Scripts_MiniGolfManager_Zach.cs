7c42495ba33c0b742b90de229f8940d3717e45b3
BianoBonzo <brianlin16@hotmail.com>
2021-11-09 14:25:42 -0500

    Created game over UI and functionality for mini golf

--- Unity_KBI_IM/Assets/Scripts/Minigames/MiniGolf/ZachTestScripts/Scripts_MiniGolfManager_Zach.cs
+++ Unity_KBI_IM/Assets/Scripts/Minigames/MiniGolf/ZachTestScripts/Scripts_MiniGolfManager_Zach.cs
@@ -14,4 +14,30 @@ public class Scripts_MiniGolfManager_Zach : Scripts_BaseManager_Wyatt
 
         Screen.orientation = ScreenOrientation.Portrait;
     }
+
+    // Game Timers
+    public GameObject globalTimer;
+    // Game Over UI  
+    public GameObject gameOverPanel;
+
+    void Update() {
+
+        int endGlobalTime = 0;
+        int.TryParse(globalTimer.GetComponent<TMPro.TextMeshProUGUI>().text, out endGlobalTime);
+
+        if (endGlobalTime <= 25) {
+            globalTimer.GetComponent<TMPro.TextMeshProUGUI>().color = Color.red;
+        }
+        if (endGlobalTime <= 20) {
+            GameOver(); 
+        } 
+    }
+
+    // <summary> 
+    // Sets global timer invisible and displays the game over panel
+    // </summary>
+    void GameOver() {
+        globalTimer.SetActive(false);
+        gameOverPanel.SetActive(true);
+    }    
 }
