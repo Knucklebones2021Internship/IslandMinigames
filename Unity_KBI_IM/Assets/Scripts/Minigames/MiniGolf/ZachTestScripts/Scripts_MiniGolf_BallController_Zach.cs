8ea0a481189b8ed6f13aabadb62a7299e0e172ed
Zachariah Abueg <76718657+zdgra@users.noreply.github.com>
2021-11-22 18:48:20 -0500

    drag and shoot finally working!
    - u can now aim and shoot anywhere on the screen
    - i changed the bounce threshold in the global physics settings from 0 to 0.2 (at 0, it was making idle things bounce)

--- Unity_KBI_IM/Assets/Scripts/Minigames/MiniGolf/ZachTestScripts/Scripts_MiniGolf_BallController_Zach.cs
+++ Unity_KBI_IM/Assets/Scripts/Minigames/MiniGolf/ZachTestScripts/Scripts_MiniGolf_BallController_Zach.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 /// <summary>
 /// this is the ball controller script
-/// full credit to the youtube video i learned and took this code from: 
-/// https://www.youtube.com/watch?v=LjLBHSU_yic
 /// </summary>
 
 // we first require that the ball have a rigidbody
 [RequireComponent(typeof(Rigidbody))]
 public class Scripts_MiniGolf_BallController_Zach : MonoBehaviour
 {
+    #region raycasting method [full credit: https://www.youtube.com/watch?v=LjLBHSU_yic]
+    /*
     // shotPower is a multiplier for the force applied on the ball
     // stopVelocity is the velocity at which the ball is considered to be stopped
     [SerializeField] private float shotPower;
@@ -23,205 +23,302 @@ public class Scripts_MiniGolf_BallController_Zach : MonoBehaviour
     private bool isAiming;
 
     private Rigidbody rb;
+    
+        /// <summary>
+        /// initialization
+        /// </summary>
+        private void Awake()
+        {
+            // initialize the rigidbody to the object's rigidbody
+            rb = GetComponent<Rigidbody>();
 
-    /// <summary>
-    /// initialization
-    /// </summary>
+            // aiming and the line renderer are initially "turned off"
+            isAiming = false;
+            lineRenderer.enabled = false;
+        }
+
+        /// <summary>
+        /// physics calculations
+        /// </summary>
+        private void FixedUpdate()
+        {
+            // if the ball's velocity is less than or equal to
+            // what is considered its stop velocity,
+            // then we will stop the ball
+            if (rb.velocity.magnitude <= stopVelocity)
+                Stop();
+
+            // if the ball is idle and a touch is detected
+            // aiming is "turned on"
+            if (isIdle)
+                if (Input.touchCount > 0)
+                    isAiming = true;
+
+            // this is where we show the aim for the ball
+            ProcessAim();
+        }
+
+        *//*private void OnMouseDown()
+        {
+            if (isIdle)
+                isAiming = true;
+        }*//*
+
+        /// <summary>
+        /// here we stop the ball
+        /// </summary>
+        private void Stop()
+        {
+            // set linear and angular velocity to zero
+            // and mark the ball as "idle"
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            isIdle = true;
+        }
+
+        /// <summary>
+        /// this is where we process touches meant for aiming the ball
+        /// </summary>
+        private void ProcessAim()
+        {
+            // if the ball is not idle, we cannot aim
+            // if aiming is "turned off", we cannot aim
+            // thus, we do not show the aim for the ball
+            if (!isAiming || !isIdle)
+                return;
+
+            // this returns the point in world space where
+            // the user has clicked the screen
+            Vector3? worldPoint = CastMouseClickRay();
+
+            // Debug.Log("World Point/Ray: " + worldPoint);
+
+            // if the world point obtained was null,
+            // then we have no aim to process/show
+            if (!worldPoint.HasValue)
+                return;
+
+            // now, use that world point to draw the
+            // line renderer representing the aim
+            DrawLine(worldPoint.Value);
+
+            // finally, if a touch is detected
+            // and a finger was lifted from the screen
+            // then shoot towards this world point
+            if (Input.touchCount > 0)
+                if (Input.touches[0].phase == TouchPhase.Ended)
+                    Shoot(worldPoint.Value);
+        }
+
+        /// <summary>
+        /// here we convert the touch position in screen space to
+        /// a touch position in world space and return the point
+        /// in world space that represents our aim
+        /// </summary>
+        /// <returns>a nullable Vector3</returns>
+        private Vector3? CastMouseClickRay()
+        {
+            #region old code from video
+            // this is code originally from the video i learned from
+            // i have since converted all of the mouse input to touch input
+
+            *//*Vector3 screenMousePositionFar = new Vector3(
+                Input.mousePosition.x,
+                Input.mousePosition.y,
+                Camera.main.farClipPlane);
+            Vector3 screenMousePositionNear = new Vector3(
+                Input.mousePosition.x,
+                Input.mousePosition.y,
+                Camera.main.nearClipPlane);*//*
+            #endregion
+
+            // if a touch is detected
+            if (Input.touchCount > 0)
+            {
+                // these are the touch positions in screen space,
+                // with the z-component being the camera's far clip plane
+                Vector3 screenTouchPositionFar = new Vector3(
+                    Input.GetTouch(0).position.x,
+                    Input.GetTouch(0).position.y,
+                    Camera.main.farClipPlane);
+                Vector3 screenTouchPositionNear = new Vector3(
+                    Input.GetTouch(0).position.x,
+                    Input.GetTouch(0).position.y,
+                    Camera.main.nearClipPlane);
+
+                // here we convert the touch positions in screen space to world space
+                Vector3 worldTouchPositionFar = Camera.main.ScreenToWorldPoint(screenTouchPositionFar);
+                Vector3 worldTouchPositionNear = Camera.main.ScreenToWorldPoint(screenTouchPositionNear);
+
+                RaycastHit hit;
+
+                // cast a ray with
+                // - an origin at the touch position in world space relative to the near clipping plane
+                // - and a direction equal to the touch position in world space between the near and far clipping planes
+                // if the ray comes in contact with a collider, return the point of contact
+                // otherwise, return null
+                if (Physics.Raycast(worldTouchPositionNear, worldTouchPositionFar - worldTouchPositionNear,
+                                    out hit, float.PositiveInfinity))
+                    return hit.point;
+                else
+                    return null;
+            }
+
+            // if no touch is detected, return null
+            return null;
+        }
+
+        /// <summary>
+        /// this is where we draw the line renderer showing the aim for the ball
+        /// </summary>
+        /// <param name="worldPoint"></param>
+        private void DrawLine(Vector3 worldPoint)
+        {
+            // create an array of points,
+            // the first being the position of the ball
+            // the second being the world point of the touch
+            Vector3[] positions = { transform.position, -worldPoint };
+            // originally:
+            // Vector3[] positions = { transform.position, worldPoint };
+            // this drew the line in the same direction as the user pulls/aims
+
+            // set the positions of the vertices of the line to be
+            // these two positions
+            // and "turn on" the line renderer to show the aim
+            lineRenderer.SetPositions(positions);
+            lineRenderer.enabled = true;
+        }
+
+        /// <summary>
+        /// this is where we shoot the ball
+        /// we take as input the world point that stores our aim
+        /// </summary>
+        /// <param name="worldPoint"></param>
+        private void Shoot(Vector3 worldPoint)
+        {
+            // first "turn off" aiming and the line renderer
+            // since we are now shooting the ball and do not
+            // need to show the aim
+            isAiming = false;
+            lineRenderer.enabled = false;
+
+            // we want to only apply a horizontal force to the ball
+            // so in this variable, we store the world point's x- and z- components
+            // and set the y-component to the ball's y-component
+            Vector3 horizontalWorldPoint = new Vector3(worldPoint.x, transform.position.y, worldPoint.z);
+
+            // the direction of our force will be the vector between
+            // the ball's position and the horizontal world point
+            Vector3 direction = (horizontalWorldPoint - transform.position).normalized;
+
+            // the strength, or magnitude, will be the distance between
+            // the ball and the horizontal world point
+            float strength = Vector3.Distance(transform.position, horizontalWorldPoint);
+
+            // finally, we apply a force to the ball with a multiplier, shotPower
+            rb.AddForce(-direction * strength * shotPower);
+            // originally:
+            // rb.AddForce(direction * strength * shotPower);
+            // this shot the ball in the same direction as the user pulls/aims
+
+            // and now the ball is not idle
+            isIdle = false;
+        }*/
+    #endregion
+
+    public float power = 10f;
+    public float maxFingerDrag = 5f;
+    public float stopVelocity = 0.1f;
+
+    private Rigidbody rb;
+    private LineRenderer line;
+    private Camera mainCam;
+
+    private Vector3 dragStartPos;
+    private Touch touch;
+    private bool isIdle;
+    private bool isAiming = false;
+
     private void Awake()
     {
-        // initialize the rigidbody to the object's rigidbody
         rb = GetComponent<Rigidbody>();
-
-        // aiming and the line renderer are initially "turned off"
-        isAiming = false;
-        lineRenderer.enabled = false;
+        line = GetComponent<LineRenderer>();
+        mainCam = Camera.main;
     }
 
-    /// <summary>
-    /// physics calculations
-    /// </summary>
     private void FixedUpdate()
     {
-        // if the ball's velocity is less than or equal to
-        // what is considered its stop velocity,
-        // then we will stop the ball
         if (rb.velocity.magnitude <= stopVelocity)
             Stop();
 
-        // if the ball is idle and a touch is detected
-        // aiming is "turned on"
         if (isIdle)
             if (Input.touchCount > 0)
                 isAiming = true;
 
-        // this is where we show the aim for the ball
-        ProcessAim();
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+                DragStart(new Vector3(touch.position.x, touch.position.y, mainCam.transform.position.y));
+
+            if (touch.phase == TouchPhase.Moved && isIdle && isAiming)
+                Dragging(new Vector3(touch.position.x, touch.position.y, mainCam.transform.position.y));
+
+            if (touch.phase == TouchPhase.Ended && isIdle && isAiming)
+                DragRelease(new Vector3(touch.position.x, touch.position.y, mainCam.transform.position.y));
+        }
     }
 
-    /*private void OnMouseDown()
-    {
-        if (isIdle)
-            isAiming = true;
-    }*/
-
-    /// <summary>
-    /// here we stop the ball
-    /// </summary>
     private void Stop()
     {
-        // set linear and angular velocity to zero
-        // and mark the ball as "idle"
         rb.velocity = Vector3.zero;
         rb.angularVelocity = Vector3.zero;
         isIdle = true;
     }
 
-    /// <summary>
-    /// this is where we process touches meant for aiming the ball
-    /// </summary>
-    private void ProcessAim()
+    private void DragStart(Vector3 screenPoint)
     {
-        // if the ball is not idle, we cannot aim
-        // if aiming is "turned off", we cannot aim
-        // thus, we do not show the aim for the ball
-        if (!isAiming || !isIdle)
-            return;
+        // Debug.Log("DragStart()");
+        // Debug.Log("touch.position: " + touch.position);
+        dragStartPos = mainCam.ScreenToWorldPoint(screenPoint);
+        // Debug.Log("dragStartPos: " + dragStartPos);
 
-        // this returns the point in world space where
-        // the user has clicked the screen
-        Vector3? worldPoint = CastMouseClickRay();
-
-        // Debug.Log("World Point/Ray: " + worldPoint);
-
-        // if the world point obtained was null,
-        // then we have no aim to process/show
-        if (!worldPoint.HasValue)
-            return;
-
-        // now, use that world point to draw the
-        // line renderer representing the aim
-        DrawLine(worldPoint.Value);
-
-        // finally, if a touch is detected
-        // and a finger was lifted from the screen
-        // then shoot towards this world point
-        if (Input.touchCount > 0)
-            if (Input.touches[0].phase == TouchPhase.Ended)
-                Shoot(worldPoint.Value);
+        line.positionCount = 1;
+        line.SetPosition(0, transform.position);
+        line.enabled = true;
     }
 
-    /// <summary>
-    /// here we convert the touch position in screen space to
-    /// a touch position in world space and return the point
-    /// in world space that represents our aim
-    /// </summary>
-    /// <returns>a nullable Vector3</returns>
-    private Vector3? CastMouseClickRay()
+    private void Dragging(Vector3 screenPoint)
     {
-        #region old code from video
-        // this is code originally from the video i learned from
-        // i have since converted all of the mouse input to touch input
+        // Debug.Log("Dragging()");
+        // Debug.Log("touch.position: " + touch.position);
+        Vector3 draggingPos = mainCam.ScreenToWorldPoint(screenPoint);
+        // Debug.Log("draggingPos: " + draggingPos);
 
-        /*Vector3 screenMousePositionFar = new Vector3(
-            Input.mousePosition.x,
-            Input.mousePosition.y,
-            Camera.main.farClipPlane);
-        Vector3 screenMousePositionNear = new Vector3(
-            Input.mousePosition.x,
-            Input.mousePosition.y,
-            Camera.main.nearClipPlane);*/
-        #endregion
-
-        // if a touch is detected
-        if (Input.touchCount > 0)
-        {
-            // these are the touch positions in screen space,
-            // with the z-component being the camera's far clip plane
-            Vector3 screenTouchPositionFar = new Vector3(
-                Input.GetTouch(0).position.x,
-                Input.GetTouch(0).position.y,
-                Camera.main.farClipPlane);
-            Vector3 screenTouchPositionNear = new Vector3(
-                Input.GetTouch(0).position.x,
-                Input.GetTouch(0).position.y,
-                Camera.main.nearClipPlane);
-
-            // here we convert the touch positions in screen space to world space
-            Vector3 worldTouchPositionFar = Camera.main.ScreenToWorldPoint(screenTouchPositionFar);
-            Vector3 worldTouchPositionNear = Camera.main.ScreenToWorldPoint(screenTouchPositionNear);
-
-            RaycastHit hit;
-
-            // cast a ray with
-            // - an origin at the touch position in world space relative to the near clipping plane
-            // - and a direction equal to the touch position in world space between the near and far clipping planes
-            // if the ray comes in contact with a collider, return the point of contact
-            // otherwise, return null
-            if (Physics.Raycast(worldTouchPositionNear, worldTouchPositionFar - worldTouchPositionNear,
-                                out hit, float.PositiveInfinity))
-                return hit.point;
-            else
-                return null;
-        }
-
-        // if no touch is detected, return null
-        return null;
+        line.positionCount = 2;
+        Vector3 offset = draggingPos - dragStartPos;
+        line.SetPosition(1, transform.position + offset);
     }
 
-    /// <summary>
-    /// this is where we draw the line renderer showing the aim for the ball
-    /// </summary>
-    /// <param name="worldPoint"></param>
-    private void DrawLine(Vector3 worldPoint)
+    private void DragRelease(Vector3 screenPoint)
     {
-        // create an array of points,
-        // the first being the position of the ball
-        // the second being the world point of the touch
-        Vector3[] positions = { transform.position, -worldPoint };
-        // originally:
-        // Vector3[] positions = { transform.position, worldPoint };
-        // this drew the line in the same direction as the user pulls/aims
+        // Debug.Log("DragRelease()");
+        // Debug.Log("touch.position: " + touch.position);
 
-        // set the positions of the vertices of the line to be
-        // these two positions
-        // and "turn on" the line renderer to show the aim
-        lineRenderer.SetPositions(positions);
-        lineRenderer.enabled = true;
-    }
-
-    /// <summary>
-    /// this is where we shoot the ball
-    /// we take as input the world point that stores our aim
-    /// </summary>
-    /// <param name="worldPoint"></param>
-    private void Shoot(Vector3 worldPoint)
-    {
-        // first "turn off" aiming and the line renderer
-        // since we are now shooting the ball and do not
-        // need to show the aim
         isAiming = false;
-        lineRenderer.enabled = false;
+        line.enabled = false;
 
-        // we want to only apply a horizontal force to the ball
-        // so in this variable, we store the world point's x- and z- components
-        // and set the y-component to the ball's y-component
-        Vector3 horizontalWorldPoint = new Vector3(worldPoint.x, transform.position.y, worldPoint.z);
+        line.positionCount = 0;
 
-        // the direction of our force will be the vector between
-        // the ball's position and the horizontal world point
-        Vector3 direction = (horizontalWorldPoint - transform.position).normalized;
+        Vector3 dragReleasePos = mainCam.ScreenToWorldPoint(screenPoint);
+        // Debug.Log("dragReleasePos: " + dragReleasePos);
 
-        // the strength, or magnitude, will be the distance between
-        // the ball and the horizontal world point
-        float strength = Vector3.Distance(transform.position, horizontalWorldPoint);
+        Vector3 force = dragStartPos - dragReleasePos;
+        Vector3 clampedForce = Vector3.ClampMagnitude(force, maxFingerDrag);
 
-        // finally, we apply a force to the ball with a multiplier, shotPower
-        rb.AddForce(-direction * strength * shotPower);
-        // originally:
-        // rb.AddForce(direction * strength * shotPower);
-        // this shot the ball in the same direction as the user pulls/aims
+        rb.AddForce(clampedForce * power, ForceMode.Impulse);
 
-        // and now the ball is not idle
         isIdle = false;
     }
 }
