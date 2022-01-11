cf2c7aea91183b9735f5b3bbe1267dd9e84fb8ab
ToanDiep47 <toandiep47@gmail.com>
2022-01-11 12:33:06 -0500

    Laser Monster Aiming to Player
    Changed the script to have the Laser monster shoot the laser towards the player.

--- Unity_KBI_IM/Assets/Scripts/Minigames/MiniGolf/Scripts_LaserMonster_MiniGolf_Brian.cs
+++ Unity_KBI_IM/Assets/Scripts/Minigames/MiniGolf/Scripts_LaserMonster_MiniGolf_Brian.cs
@@ -12,7 +12,7 @@ public class Scripts_LaserMonster_MiniGolf_Brian : MonoBehaviour
 
     public float rotSpeed; 
     public bool rotateClockwise; 
-
+    
     // Rotation timer 
     float timer = 0;
 
@@ -24,22 +24,33 @@ public class Scripts_LaserMonster_MiniGolf_Brian : MonoBehaviour
     // Timer to shoot the laser 
     float shootTimer = 3.0f; 
 
+
+    GameObject player;
+
     // <summary> 
     // Move around in a circle and shoot the laser projectile every 3 seconds 
     // </summary>     
     void Update() {
+        if (player == null){
+            player =  player = FindObjectOfType<Scripts_MiniGolf_BallController_Zach>().gameObject;
+            return;
+        }
         timer += Time.deltaTime * rotSpeed; 
         Orbit(); 
         
         if (shootTimer <= 0) {
+            print(player.transform.position);
+            Vector3 playerDirection = (player.transform.position - transform.position).normalized;
             GameObject projectile = Instantiate(laser) as GameObject; 
             projectile.transform.position = transform.position + transform.forward * 2; 
             projectile.transform.rotation = transform.rotation;
             projectile.transform.rotation *= Quaternion.Euler(90, 0, 0);
             Rigidbody rb = projectile.GetComponent<Rigidbody>(); 
-            rb.velocity = transform.forward * 5;
+            rb.velocity = playerDirection * 5;
             Destroy(projectile, 1.5f);
             shootTimer = 3.0f;
+            
+            
         } else {
             shootTimer -= Time.deltaTime; 
         }
@@ -66,4 +77,6 @@ public class Scripts_LaserMonster_MiniGolf_Brian : MonoBehaviour
         //gameObject.transform.rotation = Quaternion.Euler(0, Rotation + 180, 0);         
         transform.rotation = Quaternion.LookRotation(transform.position - centerPoint.position, Vector3.up);
     }
+
+
 }
